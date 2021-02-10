c5487a5c91986f5bdaf3a1fb70841becea674f71
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-10 20:23:23 +0530

    Added Contact Creation ability

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -4,16 +4,18 @@ import java.util.*;
 public class AddressBookSystem 
 {	
 	static Scanner sc = new Scanner(System.in);
-	static ArrayList addbook1 = new ArrayList();
+	static ArrayList<Create> addbook1 = new ArrayList<Create>();
 	
 	public static void main(String[] args)
 	{
 		boolean quit = true;
 		int choice = 0;
 		System.out.println("Welcome to AddressbOOkSystem");
+		
 		while( quit != false )
 		{
 			System.out.println("Choose option");
+			System.out.println("0. Print the address book");
 			System.out.println("1. Create a New Contact");
 			System.out.println("2. Exit");
 			
@@ -21,79 +23,87 @@ public class AddressBookSystem
 			
 			switch(choice)
 			{
+				case 0 :
+					System.out.println(addbook1);
+					break;
+					
 				case 1 :
+					System.out.println("Enter contact details");
 					contactCreation();
 					break;
+					
 				case 2 :
 					quit = false;
 					break;
-				
 			}
 		}
 		
 	}
-	 public static void contactCreation()
+	
+	public static void contactCreation()
 	 {
-		 System.out.println("Enter first name\r"); 
-		 Create.fname=sc.next();
-		 addbook1.add(Create.fname);
-		 
-		 System.out.println("Enter last name\r"); 
-		 Create.lname=sc.next();
-		 addbook1.add(Create.lname);
-		 
-		 System.out.println("Enter address\r"); 
-		 Create.address=sc.next();
-		 addbook1.add(Create.address);
-		 
-		 System.out.println("Enter state\r"); 
-		 Create.state=sc.next();
-		 addbook1.add(Create.state);
-		 
-		 System.out.println("Enter zip\r"); 
-		 Create.zip=sc.nextInt();
-		 addbook1.add(Create.zip);
-		 
-		 System.out.println("Enter phone number\r"); 
-		 Create.ph=sc.nextInt();
-		 addbook1.add(Create.ph);
-		 
-		 System.out.println("Enter email\r"); 
-		 Create.email=sc.next();
-		 addbook1.add(Create.email);
-		 
-		 System.out.println(addbook1);
+				 Create person = new Create(null, null, null, null, null, 0, 0, null);
+				 System.out.println("Enter first name\r"); 
+				 person.fname=sc.next();
+				 
+				 System.out.println("Enter last name\r"); 
+				 person.lname=sc.next();
+				 
+				 System.out.println("Enter address\r"); 
+				 person.address=sc.next();
+				 
+				 System.out.println("Enter city\r"); 
+				 person.city=sc.next();
+				 
+				 System.out.println("Enter state\r"); 
+				 person.state=sc.next();
+				 
+				 System.out.println("Enter zip\r"); 
+				 person.zip=sc.nextInt();
+				 
+				 System.out.println("Enter phone number\r"); 
+				 person.ph=sc.nextInt();
+				 
+				 System.out.println("Enter email\r"); 
+				 person.email=sc.next();
+				 
+				 addbook1.add(person);
+				 System.out.println(person.toString());
+		 	
 	 }
 
 }
 
 class Create
 {
-	static String fname;
-	static String lname;
-	static String address;
-	static String city;
-	static String state;
-	static int zip;
-	static int ph;
-	static String email;
+	 String fname;
+	 String lname;
+	 String address;
+	 String city;
+	 String state;
+	 int zip;
+	 int ph;
+	 String email;
 	
 	Create(String fname, String lname, String address, String city, String state, int zip, int ph,
 			String email)
 	{
-		Create.fname = fname;
-		Create.lname = lname;
-		Create.address = address;
-		Create.city = city;
-		Create.state = state;
-		Create.zip = zip;
-		Create.ph = ph;
-		Create.email = email;
+		this.fname = fname;
+		this.lname = lname;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.ph = ph;
+		this.email = email;
 	}
 	
-	public void display()
+	
+	public String toString()
 	{
-		System.out.println("First name : "+fname+"\rLast name : "+lname+"\rAddress :" +address+"\rcity : "
-				+city+"\rstate : "+state+"\rzip : "+zip+"\rph : "+ph+"\remail : "+email);
+		return "\rFirst name : "+this.fname+"\rLast name : "+this.lname+"\rAddress :" +this.address+"\rcity : "
+				+this.city+"\rstate : "+this.state+"\rzip : "+this.zip+"\rph : "+this.ph+"\remail : "
+				+this.email+"\r";
 	}
+	
 }
